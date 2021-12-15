a6cfb14b5c25d13500124922e23630c15af42197
Vova-fil <vfil.zp@gmail.com>
2021-12-16 00:35:41 +0200

    задание 7.1, 7.2   версия 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryItemsContainer = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryItemsContainer.addEventListener('click', onOpenModal);
+
+galleryItemsContainer.addEventListener('keydown', onEscCloseModal);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+let image;
+
+// function createImgInModal(event) {
+//   event.preventDefault();
+//   image = basicLightbox.create(
+//     `<div class="modal">
+//     <img src="${event.target.dataset.source}">
+//     </div>`
+//   );
+//   image.show();
+// }
+
+// function onEscKeyPress(e) {
+//   const ESC_KEY_CODE = 'Escape';
+//   const isEscKey = e.code === ESC_KEY_CODE;
+
+//   if (isEscKey) {
+//     image.close();
+//   }
+// }
+
+function createImgInModal(e) {
+  image = basicLightbox.create(`
+    <img src=${e.target.dataset.source}>`);
+}
+
+function onOpenModal(e) {
+  e.preventDefault();
+  window.addEventListener('keydown', onEscCloseModal);
+
+  createImgInModal(e);
+  image.show();
+}
+
+function onEscCloseModal(e) {
+  e.preventDefault();
+  window.removeEventListener('keydown', onEscCloseModal);
+
+  if (e.code === 'Escape') {
+    image.close();
+  }
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// const image = instance.element().querySelector('data-source="${original}"');
+// const closeButton = instance.element().querySelector('button');
+
+// const instance = basicLightbox.create(`
+// <div class="modal">
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+//   <img src="" />
+//   <button>Close</button>
+// </div>
+// `);
+
+// function onButton1Click() {
+//   image.src = data-source="${original}";
+//   instance.show();
+// }
+
+const galleryItemsContainer = document.querySelector('.gallery');
+
+const galleryCardMarkup = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}"
+   alt="${item.description}" />
+</a>`
+  )
+  .join('');
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
